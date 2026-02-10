af9f1e441834ac41c1765f6b6fd0c8ed94cd2a9d
xutianhe852456 <932898583@qq.com>
2026-02-10 22:44:47 +0800

    1.10

---
+++ os/target/debug/deps/os-dcd34d3605a2bd66.d
@@ -0,0 +1,5 @@
+/home/oslab/Desktop/lab1/os/target/debug/deps/os-dcd34d3605a2bd66.rmeta: src/main.rs
+
+/home/oslab/Desktop/lab1/os/target/debug/deps/os-dcd34d3605a2bd66.d: src/main.rs
+
+src/main.rs:
---
+++ study_log/Day_1.md
@@ -0,0 +1,2 @@
+#事件一：
+使用 cargo new os --bin 创建一个可执行文件  其中 值得注意的是 --bin  告诉 cargo os为可执行文件
