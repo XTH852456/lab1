a28ab3ef37417cf6ec6c28d33210f0e8dc0ac566
XTH852456 <932898583@qq.com>
2026-02-13 17:24:39 +0800

    1.13 5:24

---
+++ os/target/riscv64gc-unknown-none-elf/debug/deps/os-8073839f67b2c422.d
@@ -0,0 +1,7 @@
+/home/oslab/lab1/os/target/riscv64gc-unknown-none-elf/debug/deps/os-8073839f67b2c422.d: src/main.rs src/lang_items.rs src/entry.asm
+
+/home/oslab/lab1/os/target/riscv64gc-unknown-none-elf/debug/deps/libos-8073839f67b2c422.rmeta: src/main.rs src/lang_items.rs src/entry.asm
+
+src/main.rs:
+src/lang_items.rs:
+src/entry.asm:
